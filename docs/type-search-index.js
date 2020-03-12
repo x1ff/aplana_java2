1bc5eeca1cfa437a6bb8eb5f95aee6c5a0ff34a1
Mikhail Loginov <mikhailloginov93@gmail.com>
2020-03-12 15:03:05 +0300

    try to host javadoc on github

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calc"},{"p":"SweetGifts","l":"Candy"},{"p":"SweetGifts","l":"Chocolate"},{"p":"SweetGifts","l":"Gift"},{"p":"SweetGifts","l":"Jellybean"},{"p":"<Unnamed>","l":"MyArray"},{"p":"SweetGifts","l":"Sweetness"},{"p":"<Unnamed>","l":"TaskRunner"},{"p":"<Unnamed>","l":"Words"}]
